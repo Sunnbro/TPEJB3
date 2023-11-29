14137cccc4b77d5d1c7863d53753bbd7adb56e3b
Sunnbro <zidane-88_@Hotmail.com>
2023-11-29 19:38:00 +0100

    final

--- TPEJB3/ejbModule/packi/ServeurCentral.java
+++ TPEJB3/ejbModule/packi/ServeurCentral.java
@@ -7,17 +7,15 @@ import jakarta.persistence.NoResultException;
 import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.TypedQuery;
 
+//ORDRE D'EXECUTION: Servercentral(run on server)->server1->server2->server3->server4->server5(peu importe pour les servers)->inter->Cl2->Cl3->Cl4->Cl5->Cl6->Cl1
 
 @Stateless
-public class ServeurCentral implements IServeurCentral {//implements IServeurCentral{
-	
-	
-	
-	
+public class ServeurCentral implements IServeurCentral {
+	// on va utiliser Entitymanager pour communiquer avec La base de donnees
 	@PersistenceContext
 	EntityManager EM;
 	
-	
+	//cette methode ajoute les services si ils n'existent pas deja
 	public void addservices() {
 		try {Long count = (Long) EM.createQuery("SELECT COUNT(s) FROM TBService s WHERE s.number BETWEEN 0 AND 15")
                 .getSingleResult();
@@ -28,7 +26,8 @@ public class ServeurCentral implements IServeurCentral {//implements IServeurCen
 	            String libelle = "Service" + i;
 	            String description = "Description du Service" + i;
 	            TBService service = new TBService(id, prevId, libelle, description);
-	            EM.persist(service);}
+	            EM.persist(service);//ajoute le service
+	            }
 			}
 		}
 		
@@ -38,9 +37,9 @@ public class ServeurCentral implements IServeurCentral {//implements IServeurCen
 	}
 
 		
-	    final String url = "jdbc:mysql://localhost:3306/dbtest";
+	   // final String url = "jdbc:mysql://localhost:3306/dbtest";
 	    
-	    
+	//cette methode ajoute le server si il n'existe pas deja   
 	  public void Addserver(String id,String ip, String name, int port) throws SQLException {
 	       
 		  Long count = (Long) EM.createQuery("SELECT COUNT(s) FROM TBserver s WHERE s.name = :serverName")
@@ -48,16 +47,17 @@ public class ServeurCentral implements IServeurCentral {//implements IServeurCen
                   .getSingleResult();
 		  if (count == 0) {
 		  TBserver server = new TBserver(id , ip , name, port);
-          EM.persist(server);
+          EM.persist(server);//ajoute le server
 		  }
 		  }
 	        
-	        
+	//cette methode Renvoie les details du service( name, number , description) 
 	  public String listServices(String name) throws SQLException {
 		    TypedQuery<TBService> query = EM.createQuery("SELECT s FROM TBService s WHERE s.name = :serviceName", TBService.class);
 		    query.setParameter("serviceName", name);
 		    try {
 		        TBService s = query.getSingleResult();
+		        //concatener le resultat dans une seule chaine
 		        String p1 = s.getnumber() + " " + s.getname() + " " + s.getdescription();
 		        return p1;
 		    } catch (NoResultException e) {
